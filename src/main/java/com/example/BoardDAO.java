014124bbd292ec6940e1bd663a18f0ea7bdbb976
김동연 <gghhj159@handong.ac,kr>
2022-12-15 19:42:59 +0900

    first

--- src/main/java/com/example/BoardDAO.java
+++ src/main/java/com/example/BoardDAO.java
@@ -1,9 +1,7 @@
 package com.example;
 
-import com.example.BoardVO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
